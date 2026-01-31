f61e7bea9b5e0d473dd5510b3c06171b6bcb91ea
jahirulalam42 <jahirulalam41@gmail.com>
2026-01-31 17:29:08 +0600

    Added p style

--- src/components/reports/OverviewCard.tsx
+++ src/components/reports/OverviewCard.tsx
@@ -14,41 +14,41 @@ const OverviewCard = ({ data }: any) => {
   return (
     <div className="flex-1 h-fit w-[50%] bg-white rounded-sm p-2 lg:p-6 flex flex-col gap-2 lg:gap-6">
       <h1 className="text-xl">Overview</h1>
-      <div className="w-full flex flex-row justify-between">
+      <div className="w-full flex flex-row gap-2 justify-between">
         <div className="flex flex-col gap-4">
-          <span>${totalProfit}</span>
+          <span className="text-xs md:text-base">${totalProfit}</span>
           <p className="text-xs lg:text-sm text-gray-500">Total Profit</p>
         </div>
 
         <div className="flex flex-col gap-4">
-          <span>${totalRevenue}</span>
+          <span className="text-xs md:text-base">${totalRevenue}</span>
           <p className="text-xs lg:text-sm text-orange-400">Revenue</p>
         </div>
 
         <div className="flex flex-col gap-4">
-          <span>${totalSales}</span>
+          <span className="text-xs md:text-base">${totalSales}</span>
           <p className="text-xs lg:text-sm text-violet-500">Sales</p>
         </div>
       </div>
 
       <div className="w-full grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-6">
         <div className="flex flex-col lg:gap-4">
-          <span>${netPurchasedValue}</span>
+          <span className="text-xs md:text-base">${netPurchasedValue}</span>
           <p className="text-xs lg:text-sm text-gray-500">Net purchase value</p>
         </div>
 
         <div className="flex flex-col lg:gap-4">
-          <span>${netSalesValue}</span>
+          <span className="text-xs md:text-base">${netSalesValue}</span>
           <p className="text-xs lg:text-sm text-gray-500">Net sales value</p>
         </div>
 
         <div className="flex flex-col lg:gap-4">
-          <span>${momProfit}</span>
+          <span className="text-xs md:text-base">${momProfit}</span>
           <p className="text-xs lg:text-sm text-gray-500">MoM Profit</p>
         </div>
 
         <div className="flex flex-col lg:gap-4">
-          <span>${yoyProfit}</span>
+          <span className="text-xs md:text-base">${yoyProfit}</span>
           <p className="text-xs lg:text-sm text-gray-500">YoY Profit</p>
         </div>
       </div>
